dc23b881dd0855345f4d7c7b553497109230b640
Daniel Bryce <danbryce410@gmail.com>
2021-06-08 14:37:53 -0500

    Process paml (#2)
    * initial pass at extracting data from paml.
    
    Still needs cleanup.
    
    * cleanup and take a partial pass at constraints
    
    * edits to connect model back to paml
    
    * checkpoint on refactoring activity graph
    
    treat all activities and having a start and stop time.
    
    Seems to be mostly working as intended but I still need to review.
    
    * remove durations
    
    * compute durations
    
    * adjust so the test_generate_constraints file can compute durations
    
    also add a bit of debugging info for durations
    
    Co-authored-by: Jack Ladwig <jladwig@sift.net>

--- src/paml_check/paml_check.py
+++ src/paml_check/paml_check.py
@@ -3,6 +3,22 @@ Check a protocol for various properties, such as consistency
 """
 import pysmt.shortcuts
 
+from paml_check.activity_graph import ActivityGraph
+
+def check_doc(doc):
+    """
+    Check a paml document for temporal consistency
+    :param doc:
+    :return:
+    """
+    graph = ActivityGraph(doc)
+    graph.print_debug()
+
+    formula = graph.generate_constraints()
+    result = check(formula)
+    doc = graph.add_result(doc, result)
+    doc = graph.compute_durations(doc)
+    return doc
 
 def check(formula):
     """
