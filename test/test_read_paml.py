f750dad0570200fe9f65b860a6cf82bbeac317a1
Daniel Bryce <danbryce410@gmail.com>
2021-08-18 15:31:19 -0500

    update path for resources

--- test/test_read_paml.py
+++ test/test_read_paml.py
@@ -3,7 +3,7 @@ import sbol3
 import paml
 
 def test_read_paml():
-    paml_file = os.path.join(os.getcwd(), 'resources/paml', 'igem_ludox_draft.ttl')
+    paml_file = os.path.join(os.getcwd(), 'test/resources/paml', 'igem_ludox_draft.ttl')
     doc = sbol3.Document()
     doc.read(paml_file, 'ttl')
     protocols = doc.find_all(lambda obj: isinstance(obj, paml.Protocol))
